7f9552dd11d29d2d0f42f04ec0a86ae4e5ac9d22
KevinFromConn <kfc21194@gmail.com>
2021-06-08 14:47:28 -0400

    added variables and functions in js

--- OneDrive/Desktop/robot-gladiators/assets/js/game.js
+++ OneDrive/Desktop/robot-gladiators/assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?");
+
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this willd o math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// This creates a function named "fight"
+function fight() {
+    window.alert("The Fight Has Begun!");
+}
+
+// fight();
